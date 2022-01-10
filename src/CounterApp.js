55f3f9a099966e34c8dad11182c47bbf530a7cf1
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2022-01-10 20:16:00 +0100

    Adding handlereset and handlesubtract

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -4,16 +4,27 @@ import PropTypes from 'prop-types';
 export const CounterApp = ({value}) => {
 
     const [counter, setCounter] = useState(value)
+    
     const handleAdd = (e) => {
         setCounter(counter + 1);
-        // setCounter( (c) => c + 1);
-        
+        // setCounter( (c) => c + 1);   
     }
+
+    const handleReset = () => setCounter(value);
+
+    const handleSubtract = () => {
+
+        setCounter( (c) => (c - 1));
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
+            <hr />
             <h2> { counter } </h2>
             <button onClick= { handleAdd } className="btn btn-primary">+1</button>
+            <button onClick= { handleReset } className="btn btn-primary">Reset</button>
+            <button onClick= { handleSubtract } className="btn btn-primary">-1</button>
         </>
     )
 }
