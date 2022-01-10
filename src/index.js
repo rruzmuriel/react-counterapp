740f67d56ebf574d21670cde962cc38caef2a969
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2022-01-10 19:27:51 +0100

    Adding CounterApp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { PrimeraApp } from './PrimeraApp';
+// import { PrimeraApp } from './PrimeraApp';
+import { CounterApp } from './CounterApp';
 
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp saludo="Hola, Soy Rafa" />, divRoot);
+ReactDOM.render( <CounterApp value= {10} />, divRoot);
