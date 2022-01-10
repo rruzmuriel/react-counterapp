2e59b76f12d10b9ba23a263c1b339dddc068da13
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2022-01-10 18:02:59 +0100

    Adding changes to print vars in html

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export const PrimeraApp = () => {
+    
+    const saludo = 'Hola Mundo';
+    
+    // const saludo2 = {
+    //     nombre: 'Rafa',
+    //     Pais: 'España'
+    // }
+    
     return (
         <>
-            <h1>Hola Mundo</h1>
+            <h1>{ saludo }</h1>
+            {/* <pre>{ JSON.stringify(saludo2) }</pre> */}
             <p>Mi Primera Aplicación</p>
         </>
     )
