95b2163570442903cebf15bb3001b896929f1836
RAFAEL RUZ MURIEL <rruzmuriel@gmail.com>
2022-01-10 19:59:44 +0100

    Adding handleAdd and useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(value)
+    const handleAdd = (e) => {
+        setCounter(counter + 1);
+        // setCounter( (c) => c + 1);
+        
+    }
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
+            <button onClick= { handleAdd } className="btn btn-primary">+1</button>
         </>
     )
 }
